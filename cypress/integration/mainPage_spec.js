91cd86baeb600b72cff1438988bf9b7a91e0ebba
Stevie Ambroise <s.ambroise@protonmail.com>
2022-05-08 23:46:27 -0600

    Test functionality of input times submission

--- cypress/integration/mainPage_spec.js
+++ cypress/integration/mainPage_spec.js
@@ -3,11 +3,12 @@ describe('Main Page', () => {
     cy.visit('http://localhost:3000/')
   })
 
-  it('User should see the nav bar, the input field, submit button, and the avg minutes', () => {
+  it('User should see the nav bar, the input field which they can type into and submit information, submit button, and the avg minutes', () => {
     cy.get('.nav-bar').contains('Bitly Logo')
     .get('label').contains('Race Time:')
     .get('input').type('Hello World')
-    .get('button').contains('Submit')
+    .get('button').contains('Submit').click()
+    .get('li').contains('Hello World')
     .get('.avg-display').contains('Your Avg Time in')
   })
 
